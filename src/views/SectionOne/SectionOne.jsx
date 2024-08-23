a7cb9a5edeec7af739901ca1d1af9610b768ead7
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2024-08-23 13:52:08 +0500

    update

--- src/views/SectionOne/SectionOne.jsx
+++ src/views/SectionOne/SectionOne.jsx
@@ -4,19 +4,15 @@ export default function SectionOne() {
     return (
       <div className="main">
         <div className="container mx-auto">
-          <div className="text-[#073843] font-[700] text-[48px] w-1/2 grid gap-10 py-[50px]">
-            <h1 className="font-kaushan">
-              Buxgalteriya bo`yicha barcha xizmatlar
-            </h1>
-            <h1 className="font-kaushan">
-              Moliyaviy xisobotni Milliy standartlar (BHMS)dan xalqaro
-              standartlar (IFRS)ga o`girish
-            </h1>
-            <h1 className="font-kaushan">
-              Xisob siyosati va Biznes-reja tuzish <br />
-              Boshqaruv xisobini tashkil etish
-            </h1>
-            <button className="text-[18px] bg-[#073843] text-white py-2 px-4 w-[200px] rounded-lg hover:bg-[#2f3b3d]">
+          <div className="text-[#073843] font-[700] text-[48px] w-1/2 grid gap-14 py-[200px]">
+            <div>
+              <h1 className="">
+                "Hisob-kitob aniq bo`lsa, <br /> biznes muvaffaqiyatli bo`ladi."
+              </h1>
+              <p className="text-[38px] float-right  mr-[100px]">Genry Ford</p>
+            </div>
+
+            <button className="text-[18px] border-4 border-[#D35E25] text-[#D35E25] py-2 px-4 w-[200px] rounded-lg hover:bg-[#2f3b3d] hover:text-white hover:border-[#2f3b3d]">
               Bog`lanish
             </button>
           </div>
