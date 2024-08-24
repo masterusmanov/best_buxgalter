b3b80d6216433aeb0f6fcc7896fad8a6e60fbb3b
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2024-08-24 06:43:38 +0500

    update

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,9 +1,13 @@
+import logo2 from '../../assets/logo/logo2.svg'
 export default function Footer() {
     return (
-      <div className="bg-[#073843] py-[50px] border-t-4">
-        <div className="flex items-center gap-6 justify-center text-[#F0EAD6] text-[28px]">
+      <div className="bg-[#073843] border-t-4">
+        <div className="flex items-center gap-6 justify-center text-[#F0EAD6] text-[24px]">
           <div className="w-1/3">
-            <h1>BESTBUXGALTER.UZ</h1>
+            <div className="flex gap-6 items-center ">
+              <img src={logo2} alt="logo" className="w-[250px]" />
+              {/* <h1>BESTBUXGALTER.UZ</h1> */}
+            </div>
           </div>
           <div className="w-1/3">
             <div className="flex items-center justify-end gap-4">
@@ -15,8 +19,16 @@ export default function Footer() {
               <a href="https://t.me/best_buxgalter">@best_buxgalter</a>
             </div>
           </div>
-            </div>
-            <h1 className="text-center mt-[40px] text-[20px] text-[#F0EAD6]">©{new Date().getFullYear()} </h1>
+        </div>
+        <div className='flex items-center justify-between px-5'>
+        <h1 className="text-center text-[20px] text-[#F0EAD6]">
+          ©{new Date().getFullYear()}. Barcha huqular himoyanlangan
+        </h1>
+          <div>
+            <h1 className='text-end text-[#F0EAD6]'>Powered by</h1>
+            <p className="text-end w-full text-[#F0EAD6]"><a href="https://t.me/masterusmanov"><span> <i className="bx bxl-telegram text-blue-500"></i></span> @masterusmanov</a> <br /><span><i className="bx bx-phone"></i></span> +998972179900</p>
+          </div>
+       </div>
       </div>
     );
 }
--- src/views/SectionThree/SectionThree.jsx
+++ src/views/SectionThree/SectionThree.jsx
@@ -4,6 +4,25 @@ import standard from "../../assets/SectionFour/standard.jpg";
 import premium from "../../assets/SectionFour/premium.jpg";
 
 export default function SectionThree() {
+  const tarif = [
+    {
+      title: "Imkon",
+      description:
+        "Ushbu tarif endigina davlat ro`yxatidan o`tgan mijozlarimizga taqdim etiladi. Davlat ro`yxatidan o`tgan sanadan boshlab dastlabki 3 oy bepul xizmat ko`rsatiladi. Ushbu muddatdan so`ng keyingi qaysi tarifga o`zgarishi kelishiladi.",
+      img: imkon,
+    },
+    {
+      title: "Standart",
+      description:
+        "Ushbu tarif ishchi-hodimlar soni 5 nafargacha bo`lgan yillik aylanmasi 1 milliardgacha bo`lgan tadbirkorlar uchun mos keladi. Xizmat uchun oylik to`lov 2 mln so`m.",
+      img: standard,
+    },
+    {
+      title: "Premium",
+      description: "Ushbu tarif ishchi - hodimlar soni 5 nafardan ortiq bo`lgan, nisbatan hujjatlar aylanishi ko`proq bo`lgan va yillik aylanmasi 1 milliard so`mdan ortiqni tashkil etuvchi korxonalar uchun tavsiya etiladi.",
+      img: premium,
+    },
+  ];
   return (
     <div className="bg-[#073843]">
       <div className="container mx-auto my-[50px]">
@@ -14,41 +33,17 @@ export default function SectionThree() {
           <img src={line2} alt="line" className="h-[12px] mx-[750px]" />
         </div>
         <div className="flex items-center gap-6 py-[70px]">
-          <div className="flex gap-6 w-1/3 h-[300px] items-center  border-2 border-[#D35E25] p-5 rounded-2xl">
-            <img src={imkon} alt="" className="w-[200px] h-[220px]" />
-            <div className="text-[#F0EAD6]">
-              <h1 className="text-center text-[28px] font-[700]">"Imkon"</h1>
-              <p className="text-justify">
-                Ushbu tarif endigina davlat ro`yxatidan o`tgan mijozlarimiz-ga
-                taqdim etiladi. Davlat ro`y-xatidan o`tgan sanadan boshlab
-                dastlabki 3 oy bepul xizmat ko`-rsatiladi. Ushbu muddatdan
-                so`-ng keyingi qaysi tarifga o`zga-rishi kelishiladi.
-              </p>
+          {tarif.map((el, index) => (
+            <div key={index} className=" w-1/3 h-[300px] border-2 border-[#D35E25] p-5 rounded-2xl text-[#F0EAD6]">
+              <div className="w-full flex items-center ">
+                <img src={el.img} alt="" className="w-[120px] h-[80px] object-cover" />
+                <h1 className=" mx-auto text-[42px] font-[700]">{el.title}</h1>
+              </div>
+              <div className="text-[#F0EAD6] mt-6 text-[20px]">
+                <p className="text-justify">{ el.description }</p>
+              </div>
             </div>
-          </div>
-          <div className="flex gap-6 w-1/3 items-center h-[300px]  border-2 border-[#D35E25] p-5 rounded-2xl">
-            <img src={standard} alt="" className="w-[210px] h-[220px]" />
-            <div className="text-[#F0EAD6]">
-              <h1 className="text-center text-[28px] font-[700]">"Standart"</h1>
-              <p className="text-justify">
-                Ushbu tarif ishchi-hodimlar soni 5 nafargacha bo`lgan yillik
-                aylanmasi 1 milliardgacha bo`lgan tadbirkorlar uchun mos keladi.
-                Xizmat uchun oylik to`lov 2 mln so`m.
-              </p>
-            </div>
-          </div>
-          <div className="flex gap-6 w-1/3 items-center h-[300px] border-2 border-[#D35E25] p-5 rounded-2xl">
-            <img src={premium} alt="" className="w-[210px] h-[220px]" />
-            <div className="text-[#F0EAD6]">
-              <h1 className="text-center text-[28px] font-[700]">"Premium"</h1>
-              <p className="text-justify">
-                Ushbu tarif ishchi - hodimlar soni 5 nafardan ortiq bo`lgan,
-                nisbatan hujjatlar aylanishi ko`proq bo`lgan va yillik aylan-
-                masi 1 milliard so`mdan ortiq-ni tashkil etuvchi korxonalar
-                uchun tavsiya etiladi.
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
