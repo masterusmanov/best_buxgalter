e162ed9f5cb610a23da72719c3fa44f881cedfb7
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2024-08-28 16:18:21 +0500

    update

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -2,33 +2,49 @@ import logo2 from '../../assets/logo/logo2.svg'
 export default function Footer() {
     return (
       <div className="bg-[#073843] border-t-4">
-        <div className="flex items-center gap-6 justify-center text-[#F0EAD6] text-[24px]">
-          <div className="w-1/3">
+        <div className="md:flex items-center gap-6 justify-center text-[#F0EAD6] text-[24px]">
+          <div className="md:w-1/3">
             <div className="flex gap-6 items-center ">
-              <img src={logo2} alt="logo" className="w-[250px]" />
-              {/* <h1>BESTBUXGALTER.UZ</h1> */}
+              <img
+                src={logo2}
+                alt="logo"
+                className="w-[200px] mx-auto md:mx-0 md:w-[250px]"
+              />
             </div>
           </div>
-          <div className="w-1/3">
-            <div className="flex items-center justify-end gap-4">
-              <i className="bx bx-phone"></i>
-              <h1>+998999098015</h1>
+          <div className="md:w-1/3">
+            <div className="w-1/2 mx-auto md:w-full md:mx-0 flex items-center md:justify-end gap-4">
+              <i className="bx bx-phone text-center"></i>
+              <h1 className="">+998999098015</h1>
             </div>
-            <div className="flex items-center justify-end gap-4">
+            <div className="w-1/2 mx-auto md:w-full md:mx-0 flex items-center md:justify-end gap-4">
               <i className="bx bxl-telegram text-blue-500"></i>
               <a href="https://t.me/best_buxgalter">@best_buxgalter</a>
             </div>
           </div>
         </div>
-        <div className='flex items-center justify-between px-5'>
-        <h1 className="text-center text-[20px] text-[#F0EAD6]">
-          ©{new Date().getFullYear()}. Barcha huqular himoyanlangan
-        </h1>
-          <div>
-            <h1 className='text-end text-[#F0EAD6]'>Powered by</h1>
-            <p className="text-end w-full text-[#F0EAD6]"><a href="https://t.me/masterusmanov"><span> <i className="bx bxl-telegram text-blue-500"></i></span> @masterusmanov</a> <br /><span><i className="bx bx-phone"></i></span> +998972179900</p>
+        <div className="mt-[30px] md:mt-0 md:flex items-center justify-between md:px-5">
+          <h1 className="text-center text-[20px] text-[#F0EAD6]">
+            ©{new Date().getFullYear()}. Barcha huqular himoyanlangan
+          </h1>
+          <div className='mt-[20px] md:mt-0 mr-6 md:mr-0'>
+            <h1 className="text-end text-[#F0EAD6]">Powered by</h1>
+            <p className="text-end w-full text-[#F0EAD6]">
+              <a href="https://t.me/masterusmanov">
+                <span>
+                  {" "}
+                  <i className="bx bxl-telegram text-blue-500"></i>
+                </span>{" "}
+                @masterusmanov
+              </a>{" "}
+              <br />
+              <span>
+                <i className="bx bx-phone"></i>
+              </span>{" "}
+              +998972179900
+            </p>
           </div>
-       </div>
+        </div>
       </div>
     );
 }
--- src/views/SectionEight/SectionEight.jsx
+++ src/views/SectionEight/SectionEight.jsx
@@ -22,60 +22,46 @@ export default function SectionEight() {
        console.log("Form data submitted:", formData);
      };
   return (
-    <div className="bg-[#073843] p-10">
+    <div className="bg-[#073843] p-5 lg:p-10">
       <div className="container mx-auto">
-        <div className="bg-[#B6B4B2] flex gap-6 p-5 rounded-2xl">
-          <div className="w-1/2 border-8 rounded-2xl p-5">
-            <h1 className=" text-[#073843] text-[32px] py-6 font-[700]">
-              Savollar bormi?
-            </h1>
-            <p className="font-[600]">
-              Agar sizda buxgalteriyaga oid muammolar bo`lsa, biz bilan
-              bog`laning!
-            </p>
-            <div className="py-5">
-              <form
-                onSubmit={handleSubmit}
-                className=" mx-auto p-5 bg-[#B6B4B2] "
-              >
-                <h1 className="font-[700] text-[22px]">
-                  Ma`lumotlaringizni kiriting!
-                </h1>
-                <div className="my-[50px]">
+        <div className="bg-[#B6B4B2] lg:flex gap-6 lg:p-5 rounded-2xl">
+          <div className="lg:w-1/2 border-8 rounded-2xl p-5">
+            <h1 className=" text-[#073843] text-[24px] lg:text-[32px] py-6 font-[700]">Savollar bormi?</h1>
+            <p className="font-[600] text-[14px] md:text-[16px]"> Agar sizda buxgalteriyaga oid muammolar bo`lsa, biz bilan bog`laning!</p>
+            <div className="py-2 lg:py-5">
+              <form onSubmit={handleSubmit} className=" mx-auto p-5 bg-[#B6B4B2]">
+                <h1 className="font-[700] lg:text-[22px]">Ma`lumotlaringizni kiriting!</h1>
+                <div className="my-[15px] lg:my-[50px]">
                   <input
                     type="text"
                     name="name"
                     value={formData.name}
                     onChange={handleChange}
-                    className="w-[90%] p-2 border border-gray-300 rounded-lg"
+                    className="w-full lg:w-[90%] p-2 border border-gray-300 rounded-lg"
                     placeholder="Ismingiz! *"
                   />
                 </div>
-                <div className="my-[50px]">
+                <div className="my-[15px] lg:my-[50px]">
                   <input
                     type="tel"
                     name="phone"
                     value={formData.phone}
                     onChange={handleChange}
-                    className="w-[90%] p-2 border border-gray-300 rounded-lg"
+                    className="w-full lg:w-[90%] p-2 border border-gray-300 rounded-lg"
                     placeholder="Telefon raqamingiz! *"
                   />
                 </div>
                 <button
                   type="submit"
-                  className="float-right font-[600] bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+                  className="w-full md:w-[150px] float-right font-[600] bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
                 >
                   Yuborish
                 </button>
               </form>
             </div>
           </div>
-          <div className="w-1/2">
-            <img
-              src={operator}
-              alt=""
-              className="w-[450px] mx-auto object-cover"
-            />
+          <div className="w-1/2 hidden lg:flex">
+            <img src={operator} alt="" className="w-[450px] mx-auto object-cover"/>
           </div>
         </div>
       </div>
